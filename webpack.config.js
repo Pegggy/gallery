d75278e9a8ca3e311e07300938d40761f391198f
penfan <fangpei9212@163.com>
2017-09-25 17:20:16 +0800

    配置 webpack

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin=require("html-webpack-plugin")
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname,'./dist'),
+    filename: "[name].js"
+  },
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename : "./index.html",
+			template: "./index.html",
+			inject : "body"
+		})
+	],
+	module : {
+		rules:[
+			{
+				test:/\.js$/,
+				loader:"babel-loader",
+				options : {
+					presets:["latest","react"]
+				},
+				include:[
+					path.resolve(__dirname,"./src")
+				]
+			},{
+				test:/\.css$/,
+				loader :"style-loader!css-loader"
+			},{
+				test:/\.css$/,
+				loader :"postcss-loader",
+				options: {
+					plugins:function(loader){
+						return [require('autoprefixer')({
+							browsers: ['last 5  versions']
+						})]
+					}
+				}
+			},{
+				test: /\.(woff|svg|ttf|eot)$/i,
+				loader:'url-loader',
+				options:{
+					/*图片名称*/
+					name:"fonts/[name].[ext]",
+					/*位置*/
+				}
+			},{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				loader:'file-loader',
+				options:{
+					/*图片名称*/
+					name:"images/[name].[ext]",
+					/*位置*/
+				}
+			},{
+				test:/\.json$/,
+				loader:"json-loader"
+			},{
+				test:/\.less$/,
+				use: [{
+					loader: "style-loader" // creates style nodes from JS strings 
+				}, {
+					loader: "css-loader" // translates CSS into CommonJS 
+				}, {
+					loader: "less-loader" // compiles Less to CSS 
+				}]
+			}
+		]
+	}
+}
+}
